f152adfc018c456c4f76ad03cdb615cfcccb8fb4
Paul Reed <paul.reed@puppet.com>
2022-02-05 15:06:28 -0500

    update task

--- tasks/toCentOSVault.sh
+++ tasks/toCentOSVault.sh
@@ -10,8 +10,10 @@ RELEASE="${RELEASE_NAME##*=} ${RELEASE_VERSION##*=}"
 
 if [[ "${RELEASE}" == *"CentOS Linux"* && "${RELEASE}" == *"8"* ]]; then 
   echo "Release: ${RELEASE}"
-  #sudo sed -i -e "s|mirrorlist=|#mirrorlist=|g" -e "s|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g" /etc/yum.repos.d/CentOS-Linux-*
   echo "Updating Repos..."
+
+  sudo sed -i -e "s|mirrorlist=|#mirrorlist=|g" -e "s|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g" /etc/yum.repos.d/CentOS-Linux-*
+
   if [[ "${PT_run_yum}" == true ]]; then 
       echo "Running Yum Update..."
       yum update -y
